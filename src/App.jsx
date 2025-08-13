adda9d4694ddcaa087f62ec5632ad24c0227af1c
MuhammedZeki <muhammedzekiylmz5353@gmail.com>
2025-08-13 15:07:12 +0300

    added to packages

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import OverviewPage from "./pages/OverviewPage";
+import ProductsPage from "./pages/ProductsPage";
+import SidebarPage from "./_components/SideBar";
+function App() {
+  return (
+    <div className="h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      {/* Background */}
+      <div className="fixed inset-0 z-0">
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80"></div>
+        <div className="absolute inset-0 backdrop-blur-sm"></div>
+      </div>
+      {/* Content */}
+      <SidebarPage />
+      <Routes>
+        <Route path="/" element={<OverviewPage />} />
+        <Route path="/product" element={<ProductsPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/_components/SideBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SidebarPage = () => {
+  return <div className="text-white">SideBar</div>;
+};
+
+export default SidebarPage;
